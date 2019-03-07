2963333722931b228286c199b1a7109500083a21
Max <maksim.l1094@gmail.com>
2019-03-07 09:48:12 +0000

    Rebased & Conflicts solved.

--- components/Section.js
+++ components/Section.js
@@ -111,7 +111,6 @@ export class Section extends React.Component {
       </>
     )
   }
-
   respRateDecision = (question) => {
     return function (respRate) {
       questionId = question.resultToGoto(respRate)
@@ -119,11 +118,8 @@ export class Section extends React.Component {
       this.moveToQuestion(questionId)
     }.bind(this)
   }
-
+  /*
   answerButtons = (question) => {
-    if (question.sectionEnd && !question.answers) {
-      return <AnswerButton onPress={() => this.props.onCompletion(this.state.currentQuestionId)}>
-        <AnswerText>Next Section</AnswerText>
       </AnswerButton>
     }
     else if (question.containsFunction) {
@@ -148,6 +144,7 @@ export class Section extends React.Component {
       return <Text>Invalid Question (no answers or sectionEnd)</Text>
     }
     }
+    */
   respRateDecision = (question) => {
     return function(respRate) {
         questionId = question.resultToGoto(respRate)
@@ -156,24 +153,33 @@ export class Section extends React.Component {
     }.bind(this)
   }
 
-  questionLogic = (question) => {
-    if (question.sectionEnd) {
-        return <AnswerButton onPress={() => this.props.onCompletion(this.state.currentQuestionId)}>
-            <AnswerText>Next Section</AnswerText>
-        </AnswerButton> 
-    } else if (question.answers.length > 0) {
-        return question.answers.map((answer, index) => 
+answerButtons = (question) => {
+    if (question.sectionEnd && !question.answers)
+    {
+    return <AnswerButton onPress={() => this.props.onCompletion(this.state.currentQuestionId)}>
+        <AnswerText>Next Section</AnswerText>
+    </AnswerButton>
+    } 
+    else if (question.answers.length > 0) 
+    {
+    return question.answers.map((answer, index) =>
+        <AnswerRow key={index}>
         <AnswerButton
-        accessibilityLabel={answer.text}
-        onPress={() => this.moveToQuestion(answer.goto)}
-        key={index}
+            accessibilityLabel={answer.text}
+            onPress={() => question.sectionEnd ? 
+            this.props.onCompletion(this.state.currentQuestionId) :
+            this.moveToQuestion(answer.goto)}
         >
-        <AnswerText>{answer.text}</AnswerText>
-        </AnswerButton>)
-    } else {
-        return <Text>Invalid Question (no answers or sectionEnd)</Text>
+            <AnswerText>{answer.text}</AnswerText>
+        </AnswerButton>
+        {answer.info!=undefined && this.infoCollapsable(answer, index)}
+        </AnswerRow>)
+    } 
+    else 
+    {
+    return <Text>Invalid Question (no answers or sectionEnd)</Text>
     }
-  }
+}
 
   renderQuestion = (question) => {
     return <ScrollView>
@@ -181,7 +187,7 @@ export class Section extends React.Component {
         <Question>{question.text}</Question>
 
         <ButtonsBox>
-            {this.questionLogic(question)}
+            {this.answerButtons(question)}
         </ButtonsBox>
 
     </ScrollView>
@@ -196,7 +202,7 @@ export class Section extends React.Component {
 
   render() {
     let question = this.currentQuestion()
-
+    
     if (question.specialScreen) {
         return this.renderSpecialScreen(question);
     } else {
