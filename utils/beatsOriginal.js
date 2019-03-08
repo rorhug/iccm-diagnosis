aab7f6886e000066502f00324528df0fd0fde9e4
ConorM16 <muganc@tcd.ie>
2019-03-08 12:22:57 +0000

    Counting frequencies (Bugged)

--- utils/beatsOriginal.js
+++ utils/beatsOriginal.js
@@ -7,6 +7,7 @@ var _ = require('underscore');
 //var length = 0;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 var pcmdata = [];
 var maxArr = [];
 var amps = [];
@@ -25,7 +26,11 @@ var finalBPM = 0;
 var soundfile = "sounds/New-3Breaths.mp3"
 decodeSoundFile(soundfile, (bpm) => console.log(bpm));
 =======
+=======
+var pcmdata = [];
+>>>>>>> Counting frequencies (Bugged)
 var amps = [];
+var freqs = [];
 
 //Note: I have no rights to these sound files and they are not created by me.
 //You may downlaod and use your own sound file to further test this.
@@ -112,7 +117,9 @@ function findPeaks(pcmdata, samplerate){
       //count = count/2;
       console.log("finished sampling sound - total peaks: " + count);
       console.log("Using https://github.com/victordibia/beats for soundwave analysis")
-      printAmps(amps);
+      printArray(amps);
+      countFreq(amps,samplerate);
+      printArray(freqs);
       return;
     }
 >>>>>>> Accuracy Improvements
@@ -422,17 +429,38 @@ function checkAmplitude(value){
   if(!present)
   {
     amps.push(value);
+    freqs.push(0);
     //console.log("Pushed: " + value);
   }
   return;
 }
 
-function printAmps(array){
-  console.log("Length of Amps: " + array.length);
+function printArray(array){
+  console.log("Length of Array: " + array.length);
   for(var i = 0; i < array.length; i++)
   {
     console.log((i + 1) + ". Value: " + array[i]);
   }
   return;
 }
+<<<<<<< HEAD
 >>>>>>> Analysing amplitudes
+=======
+
+function countFreq(amps,samplerate){
+  var interval = 0.05 * 1000 ; index = 0 ;
+  var step = Math.round( samplerate * (interval/1000) );
+  for(var i = index; i < index + step ; i++)
+  {
+    max = pcmdata[i] > max ? pcmdata[i].toFixed(1)  : max ;
+    for(var j = 0; j < amps.length; j++)
+    {
+      if(max == amps[j])
+      {
+        freqs[j] = freqs[j] + 1;
+      }
+    }
+  }
+  index += step ;
+}
+>>>>>>> Counting frequencies (Bugged)
