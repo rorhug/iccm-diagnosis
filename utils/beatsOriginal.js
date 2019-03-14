33b33046056232213e861026df4e30f0621cb5dc
ConorM16 <muganc@tcd.ie>
2019-03-14 12:18:41 +0000

    Runtime Improved + Coomments Added

--- utils/beatsOriginal.js
+++ utils/beatsOriginal.js
@@ -1,5 +1,4 @@
 var AudioContext = require('web-audio-api').AudioContext
-//var OfflineAudioContext = require('web-audio-api').OfflineAudioContext
 context = new AudioContext
 var fs = require('fs')
 var exec = require('child_process').exec;
@@ -16,12 +15,8 @@ var average = 0;
 var deviation = 0;
 var finalBPM = 0;
 
-//Note: I have no rights to these sound files and they are not created by me.
-//You may downlaod and use your own sound file to further test this.
-//
 var soundfile = "sounds/New-3Breaths.mp3"
 decodeSoundFile(soundfile, (bpm) => console.log(bpm));
-//printAmps(amps);
 
 /**
  * [decodeSoundFile Use web-audio-api to convert audio file to a buffer of pcm data]
@@ -33,31 +28,21 @@ function decodeSoundFile(soundfile, done){
     if (err) throw err
     context.decodeAudioData(buf, function(audioBuffer) {
       console.log(audioBuffer.numberOfChannels, audioBuffer.length, audioBuffer.sampleRate, audioBuffer.duration);
-//      length =audioBuffer.length;
       pcmdata = (audioBuffer.getChannelData(0)) ;
       samplerate = audioBuffer.sampleRate;
       maxvals = [] ; max = 0 ;
-//      playsound(soundfile)
-/*      var filter = context.createBiquadFilter();
-      //filter.type is defined as string type in the latest API. But this is defined as number type in old API.
-      filter.type = (typeof filter.type === 'string') ? 'lowpass' : 0; // LOWPASS
-      filter.frequency.value = 5000;
-      // Connect source to filter, filter to destination.
-      source.connect(filter);
-      filter.connect(context.destination);
-*/
-      //pcmdata = magnifySound(pcmdata);
+
       findPeaks(pcmdata, samplerate, done);
-      //console.log("Called print");
+
     }, function(err) { throw err })
   })
-  return; /*Integer*/
+  return;
 }
 
 
 
 /**
- * [findPeaks Naive algo to identify peaks in the audio data, and wave]
+ * [findPeaks runs through pcmdata, adding every amplitude to global amps array]
  * @param  {[type]} pcmdata    [description]
  * @param  {[type]} samplerate [description]
  * @return {[type]}            [description]
@@ -67,75 +52,20 @@ function findPeaks(pcmdata, samplerate, done){
   var interval = 0.05 * 1000 ; index = 0 ;
   var step = Math.round( samplerate * (interval/1000) );
   var max = 0 ;
-//  var prevmax = 0 ;
-//  var prevdiffthreshold = 0.4;
-//  var count = 0;
-//  var aboveAverage = false;
-//  var aboveCount = 0;
-//  var cooldown = 0;
 
-  //loop through song in time with sample rate
-  var samplesound = setInterval(function() {
-    if (index >= pcmdata.length) {
-      clearInterval(samplesound);
-//      count = bpm(count);
-//      console.log("finished sampling sound - total breaths: " + count + " bpm");
-//      console.log("Using https://github.com/victordibia/beats for soundwave analysis")
-    //  printArray(amps);
-      countFreq(amps);
-    //  printArray(freqs);
-      printResults();
-      playsound(soundfile);
-      loopThrough(pcmdata, samplerate, done);
-      return;
-    }
-
+  while(index < pcmdata.length)
+  {
     for(var i = index; i < index + step ; i++){
-      max = pcmdata[i] > max ? pcmdata[i]/*.toFixed(1)*/  : max ;
-      //console.log(max);
+      max = pcmdata[i] > max ? pcmdata[i] : max ;
       maxArr.push(max);
       checkAmplitude(max);
     }
-
-    // Spot a significant increase? Potential peak
-/*    bars = getbars(max) ;
-    if(max > average/* && cooldown == 0)
-    {
-/*      if(!aboveAverage)
-      {
-        /*if(aboveCount >= 1)
-        {
-          count = count + 1;
-        }
-        aboveAverage = true;
-      }
-      aboveCount++;
-      bars = bars + " == peak == " ;
-    }
-    else/* if(max < average)*/
-    {
-/*      if(aboveAverage && aboveCount >= 2)
-      {
-        count++;
-      }
-      bars = bars + " == trough == (" + max + ")";
-/*      aboveAverage = false;
-      aboveCount = 0;*/
-    }
-/*    bars = bars + " -- " + count + " -- ";
-*/    // Print out mini equalizer on commandline
-//    console.log(bars, max )
-//    prevmax = max ;
     max = 0 ; index += step ;
-  }, interval,pcmdata);
-}
-
-/**
- * TBD
- * @return {[type]} [description]
- */
-function detectBeats(){
-
+  }
+      countFreq(amps);
+      printResults();
+      loopThrough(pcmdata, samplerate, done);
+      return;
 }
 
 /**
@@ -163,12 +93,17 @@ function playsound(soundfile){
     if (error !== null) {
       console.log('exec error: ' + error);
     }else {
-      //console.log(" finshed ");
+      //console.log(" finished ");
       //micInstance.resume();
     }
   });
 }
 
+/**
+ * [Magnifies the pcmdata such that it can be easily analysed]
+ * @param {[float]} pcmdata [array to be magnified]
+ * @return {[float]} pcmdata [modified array]
+ */
 function magnifySound(pcmdata){
   for(var i = 0; i < pcmdata.length; i++)
   {
@@ -177,9 +112,13 @@ function magnifySound(pcmdata){
   return pcmdata;
 }
 
+/**
+ * [Checks if amplitude provided is member of global amps array, if not - add to array]
+ * @param {int} value         [value to be checked]
+ * @return {[type]}           [void]
+ */
 function checkAmplitude(value){
   var present = false;
-  //console.log("Called 1");
   for(var i = 0; i < amps.length; i++)
   {
     if(value == amps[i])
@@ -190,12 +129,17 @@ function checkAmplitude(value){
   if(!present)
   {
     amps.push(value);
+// adds new frequency for each new amplitude measured
     freqs.push(0);
-    //console.log("Pushed: " + value);
   }
   return;
 }
 
+/**
+* [Prints array]
+* @param {[float]} array     [array of floats to be printed]
+* @return {[type]}           [void]
+*/
 function printArray(array){
   console.log("Length of Array: " + array.length);
   for(var i = 0; i < array.length; i++)
@@ -205,6 +149,11 @@ function printArray(array){
   return;
 }
 
+/**
+ * [Counts the Frequency of each amplitude in amps array]
+ * @param {[float]} amps      [array of amplitudes]
+ * @return {[type]}           [void]
+ */
 function countFreq(amps){
   for(var j = 0; j < maxArr.length; j++)
   {
@@ -219,16 +168,22 @@ function countFreq(amps){
   return;
 }
 
+/**
+* [prints the total amount of frequencies measured + average amplitude]
+* @param {[type]}            [void]
+* @return {[type]}           [void]
+*/
 function printResults(){
-/*  for(var i = 0; i < amps.length; i++)
-  {
-    console.log("Amplitude: " + amps[i] + " - Frequency: " + freqs[i]);
-  }*/
   totalFreqs();
   weightedAverage(amps,freqs,total);
   return;
 }
 
+/**
+ * [Counts total number of frequencies measured]
+ * @param {[type]}            [void]
+ * @return {[type]}           [void]
+ */
 function totalFreqs(){
   total = 0;
   for(var i = 0; i < freqs.length; i++)
@@ -238,6 +193,13 @@ function totalFreqs(){
   console.log("Total frequencies: " + total);
 }
 
+/**
+ * [calculates weighted average amplitude]
+ * @param {[float]} amps        [array of amplitudes measured from pcmdata]
+ * @param {[float]} freqs       [frequency of each amplitude]
+ * @param {int} total           [total amount of frequencies]
+ * @return {[type]}           [void]
+ */
 function weightedAverage(amps,freqs,total){
   average = 0;
   for(var i = 0; i < amps.length; i++)
@@ -248,28 +210,41 @@ function weightedAverage(amps,freqs,total){
   return;
 }
 
+/**
+ * [Removes double count for inhale and exhale]
+ * @param {int} count     [total number of breaths measured]
+ * @return {int} count    [new breath amount]
+ */
 function bpm(count){
   count = count/2;
-//  count = count*6;
   return count;
 }
 
+/**
+ * [Calculate standard deviation of all amplitudes measured]
+ * @param {[floats]} pcmdata        [all data from sound file]
+ * @return {[type]}                 [void]
+ */
 function standardDeviation(pcmdata){
-//  var deviations = [];
   var result = 0;
   for(var i = 0; i < amps.length; i++)
   {
     result = amps[i] - average;
     result = Math.pow(result, 2);
     result = result*freqs[i];
-//    deviations.push(result);
-//    console.log(result);
     deviation = deviation + (result/total);
   }
   //deviation = Math.sqrt(deviation);
   console.log("Deviation: " + deviation);
+  return;
 }
 
+/**
+ * [Loops through soundfile again, computes breaths per minute]
+ * @param {[float]} pcmdata         [array of all data from sound file]
+ * @param {float} samplerate        [sampling rate of sound file]
+ * @param {[function]} done         [final BPM value to be passed to app]
+ */
 function loopThrough(pcmdata,samplerate,done){
 
     var interval = 0.05 * 1000 ; index = 0 ;
@@ -281,63 +256,41 @@ function loopThrough(pcmdata,samplerate,done){
     var aboveAverage = false;
     var aboveCount = 0;
     standardDeviation(pcmdata);
-  //  var cooldown = 0;
 
-    //loop through song in time with sample rate
-    // var samplesound = setInterval(function() {
-    while(1) {
-      if (index >= pcmdata.length) {
-        //clearInterval(samplesound);
-        finalBPM = bpm(count);
-        done(finalBPM)
-        console.log("finished sampling sound - total breaths: " + count + " bpm");
-        console.log("Using https://github.com/victordibia/beats for soundwave analysis")
-      //  printArray(amps);
-      //  countFreq(amps);
-      //  printArray(freqs);
-      //  printResults();
-        return;
+    while(index < pcmdata.length) {
+      for(var i = index; i < index + step ; i++){
+        max = pcmdata[i] > max ? pcmdata[i] : max ;
       }
 
-      for(var i = index; i < index + step ; i++){
-        max = pcmdata[i] > max ? pcmdata[i]/*.toFixed(1)*/  : max ;
-/*        //console.log(max);
-        maxArr.push(max);
-        checkAmplitude(max);
-*/      }
-
-      // Spot a significant increase? Potential peak
       bars = getbars(max) ;
       if(max > (average + deviation)/*0.0128(average*0.4029)/* && cooldown == 0*/)
       {
         if(!aboveAverage)
         {
-          /*if(aboveCount >= 1)
-          {
-            count = count + 1;
-          }
-          */
           aboveAverage = true;
         }
         aboveCount++;
-        bars = bars + " == peak == "/* + aboveCount + ")"*/ ;
+        bars = bars + " == peak == " ;
       }
-      else/* if(max < average)*/
+      else
       {
         if(aboveAverage && aboveCount >= 2)
         {
           count++;
         }
-        bars = bars + " == trough == "/* + max + ")"*/;
+        bars = bars + " == trough == ";
         aboveAverage = false;
         aboveCount = 0;
       }
       bars = bars + " -- " + count + " -- ";
-      // Print out mini equalizer on commandline
-//      console.log(bars, max )
       prevmax = max ; max = 0 ; index += step ;
     }
-  }
-
+    //clearInterval(samplesound);
+    finalBPM = bpm(count);
+    done(finalBPM)
+    console.log("finished sampling sound - total breaths: " + finalBPM + " bpm");
+    console.log("Using https://github.com/victordibia/beats for soundwave analysis")
+    return;
+}
 
   // export default decodeSoundFile;
