870e2ebf3fac1b553d2e7765766f70fa66c3db30
ConorM16 <muganc@tcd.ie>
2019-03-31 21:40:44 +0100

    Title Changes

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -14,9 +14,18 @@ const Container = styled.View`
   flex: 1;
   background-color: #fff;
   padding: 10px;
-  margin-top: 10px;
-  align-items: center;
+  /*margin-top: 10px;
+  align-items: center;*/
   justify-content: flex-start;
+`
+const ImageView = styled.View`
+  flex = 1;
+  background-color: #fff;
+  padding: 5px;
+  flex-direction: 'row';
+  /*margin-top: 10px;*/
+  align-items: center;
+  /*justify-content: space-evenly;*/
 `
 
 const SubHeading = styled.Text`
@@ -41,24 +50,26 @@ export default class InfoScreen extends React.Component {
   render() {
     return (
       <Container>
+        <ScrollView style={{ paddingLeft: 0, paddingRight: 0 }}>
+          <ImageView>
+            <Image
+            source={require('../assets/images/DD.png')}
+            style={{width: 310, height: 100,justifyContent: 'flex-start'}}
+            />
 
-        <Image
-        source={require('../assets/images/logo.png')}
-        style={{width: 100, height: 100}}
-        />
+            <Image
+            source={require('../assets/images/MSFLogo.png')}
+            style={{width: 213, height: 80,justifyContent: 'flex-end'}}
+            />
+          </ImageView>
 
-        <Image
-        source={require('../assets/images/name.png')}
-        style={{width: 200, height: 80}}
-        />
+          <SubHeading>Child Diagnosis</SubHeading>
 
-        <SubHeading>Symptom Diagnosis</SubHeading>
+          <Paragraph>This is a simple testing application for providing quick and decisive medical diagnosis.
+           By following each section
+          of the survey, a number of referrals may be provided to you based off your symptoms.</Paragraph>
 
-        <Paragraph>This is a simple testing application for providing quick and decisive medical diagnosis.
-         By following each section
-        of the survey, a number of referrals may be provided to you based off your symptoms.</Paragraph>
-        <Paragraph>By following the referral(s) provided to you, we hope that illness can be
-        recognised and helped before it reaches a crucial stage. </Paragraph>
+        </ScrollView>
 
     </Container>
     );
